29067e484b45669999f81f70cd9d697a1f4a3e5d
Nishad Agrawal <nishadagrawal7@gmail.com>
2015-11-07 16:35:47 -0500

    Created teacher page and import student data from csv into Mongo

--- GroupProject/client/templates/teacher.js
+++ GroupProject/client/templates/teacher.js
@@ -1,15 +1,54 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
 Template.teacher.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
+
 });
 
 Template.teacher.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
+  "click #createClass": function (event) {
+    var className = $("#className").val();
+    var teacherEmail = $("#teacherEmail").val();
+    var groupSize = $("#groupSize").val();
+    var isChecked = $("#randomize").is(':checked');
+
+    var reader = new FileReader();
+      // Read file into memory as UTF-8
+    reader.readAsText($("#classData").prop("files")[0]);
+
+    reader.onload = function(event) {
+      var classID = Classes.insert({
+        name: className,
+        email: teacherEmail,
+        preferences: {
+          randomize: isChecked
+        },
+        students: []
+      });
+
+      var csv = event.target.result;
+      var allTextLines = csv.split(/\r\n|\n/);
+      var studentIDs = [];
+      for (var i=0; i < allTextLines.length; i++) {
+          var data = allTextLines[i].split(',');
+          var student = {
+            name: data[0],
+            classID: classID,
+            grade: parseInt(data[1]),
+            friends: [],
+            known: [],
+            disklike: [],
+            comfort: -1
+          };
+          var id = Students.insert(student);
+          studentIDs.push(id);
+      }
+
+      Classes.update(classID, {$addToSet: {students: {$each: studentIDs}}});
+      console.log("finished creating class!");
+    };
+
+    reader.onerror = function(evt) {
+      if(evt.target.error.name == "NotReadableError") {
+          alert("Canno't read file !");
+      }
+    };
   }
 });
