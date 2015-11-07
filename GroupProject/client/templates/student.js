74a11060306a9a7184af6bc8673110d32659e446
Cassidee Kido <crk28@duke.edu>
2015-11-07 16:34:38 -0500

    Made HTML and retrieved variables in JS

--- GroupProject/client/templates/student.js
+++ GroupProject/client/templates/student.js
@@ -1,15 +1,17 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.student.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
 Template.student.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
+  'click #submit': function () {
+    var kid = $('#studentname').val();
+	var email = $('#studentemail').val();
+	var level = $('comfort').val();
+	var f1 = $('friend1').val();
+	var f2 = $('friend2').val();
+	var f3 = $('friend3').val();
+	var k1 = $('known1').val();
+	var k2 = $('known2').val();
+	var k3 = $('known3').val();
+	var d1 = $('dislike1').val();
+	var d2 = $('dislike2').val();
+	var d3 = $('dislike3').val();
+	console.log(email);
   }
 });
